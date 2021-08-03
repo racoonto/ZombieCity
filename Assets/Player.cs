4dc13d5daffcde27da3809ffc60439515e6498cc
nahyunji <racoonto@gmail.com>
2021-08-03 16:21:29 +0900

    총쏘기

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public partial class Player : MonoBehaviour
 {
     private Animator animator;
 
@@ -13,6 +14,46 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        if (Time.deltaTime == 0)
+            return;
+
+        LookAtMouse();
+        Move();
+        Fire();
+    }
+
+    private Plane plane = new Plane(new Vector3(0, 1, 0), 0);
+
+    private void LookAtMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (plane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            Vector3 dir = hitPoint - transform.position;
+            dir.y = transform.position.y;
+            dir.Normalize();
+            transform.forward = dir;
+        }
+    }
+
+    //private void Fire()
+    //{
+    //    if (Input.GetKey(KeyCode.Mouse0))
+    //    {
+    //        //animator.Play("Shoot");
+    //        animator.SetBool("Fire", true);
+    //        Instantiate(bullet, bulletPosition.position, transform.rotation);
+    //    }
+    //    else
+    //    {
+    //        animator.SetBool("Fire", false);
+    //    }
+    //}
+
+    private void Move()
+    {
         Vector3 move = Vector3.zero;
         if (Input.GetKey(KeyCode.W)) move.z = 1;
         if (Input.GetKey(KeyCode.S)) move.z = -1;
@@ -22,13 +63,7 @@ public class Player : MonoBehaviour
         {
             move.Normalize();
             transform.Translate(move * speed * Time.deltaTime, Space.World);
-            transform.forward = move;
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            animator.Play("Shoot");
-            Instantiate(bullet, bulletPosition.position, transform.rotation);
+            //transform.forward = move;
         }
 
         animator.SetFloat("DirX", move.x);
@@ -37,6 +72,6 @@ public class Player : MonoBehaviour
     }
 
     public float speed = 5;
-    public GameObject bullet;
-    public Transform bulletPosition;
+    //public GameObject bullet;
+    //public Transform bulletPosition;
 }
---
+++ Assets/Player_Fire.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class Player : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPosition;
+
+
+    float shootDelayEndTime;
+    void Fire()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (shootDelayEndTime < Time.time)
+            {
+                StartCoroutine(FlashBulletCo());
+                animator.SetBool("Fire", true);
+                shootDelayEndTime = Time.time + shootDelay;
+                IncreaseRecoil();
+                Instantiate(bullet, bulletPosition.position, CalculateRecoil(transform.rotation));
+            }
+        }
+        else
+        {
+            animator.SetBool("Fire", false);
+            DecreaseRecoil();
+        }
+    }
+
+    GameObject bulletLight;
+    public float bulletFlashTime = 0.001f;
+    private IEnumerator FlashBulletCo()
+    {
+        bulletLight.SetActive(true);
+        yield return new WaitForSeconds(bulletFlashTime);
+        bulletLight.SetActive(false);
+    }
+
+    float recoilValue = 0f;
+    float recoilMaxValue = 1.5f;
+    float recoilLerpValue = 0.1f;
+    void IncreaseRecoil()
+    {
+        recoilValue = Mathf.Lerp(recoilValue, recoilMaxValue, recoilLerpValue);
+    }
+    void DecreaseRecoil()
+    {
+        recoilValue = Mathf.Lerp(recoilValue, 0, recoilLerpValue);
+
+    }
+
+    Vector3 recoil;
+    Quaternion CalculateRecoil(Quaternion rotation)
+    {
+        recoil = new Vector3(Random.Range(-recoilValue, recoilValue), Random.Range(-recoilValue, recoilValue), 0);
+        return Quaternion.Euler(rotation.eulerAngles + recoil);
+    }
+
+    [SerializeField] float shootDelay = 0.05f;
+}
