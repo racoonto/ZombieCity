ad35b2052fdba7843052a9e959031aad9be3e547
nahyunji <racoonto@gmail.com>
2021-08-04 20:54:11 +0900

    UI

---
+++ Assets/StageManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : SingletonMonoBehavior<StageManager>
+{
+    public SaveInt highScore;
+
+    new private void Awake()
+    {
+        base.Awake();
+    }
+}
