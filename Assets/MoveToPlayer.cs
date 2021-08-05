4e4c1b4983ae8429955b55e9449aa95529b408ab
nahyunji <racoonto@gmail.com>
2021-08-05 16:42:58 +0900

    코인 캐릭터한테 이동

---
+++ Assets/MoveToPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using DG.Tweening;
+
+public class MoveToPlayer : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public float maxSpeed = 20;
+    public float duration = 3;
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            agent = GetComponent<NavMeshAgent>();
+
+            DOTween.To(() => agent.speed, (x) => agent.speed = x, maxSpeed, duration);
+
+            while (true)
+            {
+                agent.destination = other.transform.position;
+                yield return null;
+            }
+        }
+    }
+}
