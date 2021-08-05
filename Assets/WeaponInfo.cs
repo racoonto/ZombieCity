d019beae1aa1604b26bb6ad917b50a2dcc192186
nahyunji <racoonto@gmail.com>
2021-08-05 20:25:18 +0900

    야구 방맹이

---
+++ Assets/WeaponInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponInfo : MonoBehaviour
+{
+    public int damage = 20;
+    public AnimatorOverrideController overrideAnimator;
+}
