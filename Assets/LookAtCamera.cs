4692353ba89397069b5807210038df7192dfd6f3
nahyunji <racoonto@gmail.com>
2021-08-10 17:00:35 +0900

    말풍선 LookAtCamera

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+[RequireComponent(typeof(LookAtConstraint))]
+public class LookAtCamera : MonoBehaviour
+{
+    private void Awake()
+    {
+        var source = new ConstraintSource();
+        source.sourceTransform = Camera.main.transform;
+        source.weight = 1;
+
+        var loockAtConstraint = GetComponent<LookAtConstraint>();
+        loockAtConstraint.AddSource(source);
+        loockAtConstraint.constraintActive = true;
+    }
+}
