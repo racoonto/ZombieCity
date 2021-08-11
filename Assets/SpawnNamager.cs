bff0ba693da79e8ef979c0450f602bc382850469
nahyunji <racoonto@gmail.com>
2021-08-11 20:26:30 +0900

    좀비 10마리  스폰

---
+++ Assets/SpawnNamager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNamager : MonoBehaviour
+{
+    public int spawnCount = 10;
+    public GameObject monster;
+
+    private void Start()
+    {
+        var spawnPoints = GetComponentsInChildren<SpawnPoint>(true);
+        for (int i = 0; i < spawnCount; i++)
+        {
+            int spawnIndex = Random.Range(0, spawnPoints.Length);
+            Vector3 spawnPoint = spawnPoints[spawnIndex].transform.position;
+            Instantiate(monster, spawnPoint, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+    }
+}
