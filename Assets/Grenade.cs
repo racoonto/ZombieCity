466718a6bc2e57f3b8e809238fa8d231f403965e
nahyunji <racoonto@gmail.com>
2021-08-12 18:12:01 +0900

    수류탄 폭발 이펙트

---
+++ Assets/Grenade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public int power = 40;
+    public float destroyDelay = 3;
+    public float damageArea = 5;
+    public GameObject destroyEffect;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        //좀비들한테 대미지 주자.
+
+        var attackables = Physics.OverlapSphere(transform.position, damageArea, 1 << LayerMask.NameToLayer("Attackable"));
+        foreach (var item in attackables)
+        {
+            var moveDirection = transform.position - item.transform.position;
+            var zombie = item.GetComponent<Zombie>();
+            if (zombie)
+                zombie.TakeHit(power, moveDirection);
+        }
+
+        //폭발 이펙트 표시
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+    }
+}
--- Assets/GrenadeLauncher.cs
+++ Assets/GrenadeLauncher.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Granade : MonoBehaviour
+public class GrenadeLauncher : MonoBehaviour
 {
     public ProjectileArc projectileArc;
     public Transform firePoint;
-    public float currentAngle;
     public Cursor cursor;
     public float speed = 20;
 
@@ -20,21 +19,38 @@ public class Granade : MonoBehaviour
     private void Update()
     {
         SetTargetWithSpeed(cursor.transform.position, speed);
+
+        // 수류탄 발사
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            //수류탄 생성.
+            //리지드 바디에 포스를 줘서 날리자.
+            var newGrenadeGo = Instantiate(grenadeGo, firePoint.position, Quaternion.identity);
+            newGrenadeGo.transform.forward = direction;
+            float degree = -currentAngle * Mathf.Rad2Deg;
+            newGrenadeGo.transform.Rotate(degree, 0, degree);
+            Rigidbody _rigidbody = newGrenadeGo.GetComponent<Rigidbody>();
+            _rigidbody.velocity = newGrenadeGo.transform.forward * speed;
+            _rigidbody.AddTorque(Random.Range(0, torque), Random.Range(0, torque), Random.Range(0, torque));
+        }
     }
+
+    public float torque = 100;
+
+    public GameObject grenadeGo;
+    public float currentAngle;
+    private Vector3 direction;
 
     public void SetTargetWithSpeed(Vector3 point, float speed)
     {
-        Vector3 direction = point - firePoint.position;
+        direction = point - firePoint.position;
         float yOffset = direction.y;
         direction = Math3d.ProjectVectorOnPlane(Vector3.up, direction);
         float distance = direction.magnitude;
-
         float angle0, angle1;
         bool targetInRange = ProjectileMath.LaunchAngle(speed, distance, yOffset, Physics.gravity.magnitude, out angle0, out angle1);
-
         if (targetInRange)
             currentAngle = angle0;
-
         projectileArc.UpdateArc(speed, distance, Physics.gravity.magnitude, currentAngle, direction, targetInRange);
     }
 }
